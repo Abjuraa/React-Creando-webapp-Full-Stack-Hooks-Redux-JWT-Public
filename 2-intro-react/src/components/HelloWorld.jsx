b5d7c91571a18ab8f567e7ef5080da6aa71f6704
Abjura <bryamccuervo2004@gmail.com>
2024-05-22 20:36:12 -0500

    Conociendo React nativo y DOM

--- 2-intro-react/src/components/HelloWorld.jsx
+++ 2-intro-react/src/components/HelloWorld.jsx
@@ -1,8 +1,26 @@
-export function HelloWorld() {
+
+import PropTypes from 'prop-types';
+export const HelloWorld = ({user, id, title, book}) => {
+
+    console.log(title)
+    // const name = 'pepe';
+    
     return (
         <div>
-            <h1>Hola mundo</h1>
-            <div>Que tal!</div>
+            <h1>{title}</h1>
+            <div>Que tal! {user.name} {user.lastname} con el id {id}</div>
+            <div>{book}</div>
         </div>
     );
+}
+
+HelloWorld.propTypes = {
+    title: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    user: PropTypes.object.isRequired,
+}
+
+HelloWorld.defaultProps = {
+    title: 'Hola Mundo por defecto!',
+    book: 'Libro por defecto!'
 }
--- 2-intro-react/src/main.jsx
+++ 2-intro-react/src/main.jsx
@@ -5,7 +5,11 @@ import { HelloWorld } from './components/HelloWorld'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelloWorld />
+    <HelloWorld
+      user = {{name: 'Pepe', lastname: 'Doe'}}
+      id = {1}
+      title = 'Hola Mundo'
+      />
   </React.StrictMode>,
   
 )
