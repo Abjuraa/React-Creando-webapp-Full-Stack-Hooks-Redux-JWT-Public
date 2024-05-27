3d608d3131085af3431745099adab84caf18157d
Abjura <bryamccuervo2004@gmail.com>
2024-05-26 20:04:53 -0500

    Se completo el curso sobre incrementar con useState

--- 3-counter-app/src/CounterApp.jsx
+++ 3-counter-app/src/CounterApp.jsx
@@ -1,4 +1,13 @@
-export const CounterApp = () => {
+import { useState } from "react";
+
+export const CounterApp = ({value}) => {
+
+    const [counter, setCounter] = useState(value);
+
+    const counterIncrement = () => setCounter(c => c + 1);
+
     return <>
+    <h2>El valor del contador es {counter}</h2>
+        <button onClick={counterIncrement}>Incrementar Contador + 1</button>
     </>
 }
